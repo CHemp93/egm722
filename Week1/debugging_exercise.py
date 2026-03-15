40bb25649cc118c0136609de4e803f47dd4f949c
CHemp93 <Hempsey-C@ulster.ac.uk>
2026-03-15 16:22:06 +0000

    added_code_count
    - added a counter
    - counter added one with each guess
    - printed off counter when guesses correct

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,6 +5,7 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+counter = 1 # counter starts at 1
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
@@ -14,6 +15,7 @@ while guess != rand:  # if the guess is not equal to the random number, you have
 
     print('Enter a new guess: ')
     guess = int(input())
-
+    counter = counter + 1 # adds a 1 as another guess made
 
 print('You got it! The number was {}'.format(rand))
+print('guesses made were {}'.format(counter))
