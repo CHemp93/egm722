b7f7755b390a404318e4f4e1c08ff3bb86d4f2cf
CHemp93 <Hempsey-C@ulster.ac.uk>
2026-04-27 14:50:25 +0100

    updated exercise 3 script
    attempt at summary stats for exercise script update

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -7,13 +7,18 @@ import matplotlib.patches as mpatches
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-# in this section, write the script to load the data and complete the main part of the analysis.
-# try to print the results to the screen using the format method demonstrated in the workbook
+Counties = gpd.read_file('data_files/Counties.shp')# in this section, write the script to load the data and complete the main part of the analysis.
+Wards = gpd.read_file('data_files/NI_Wards.shp')# try to print the results to the screen using the format method demonstrated in the workbook
+Counties_itm = Counties.to_crs(epsg=2157) # load the necessary data here and transform to a UTM projection
+Wards_itm = Wards.to_crs(epsg=2157)
+My_join=gpd.sjoin(Counties_itm,Wards_itm,how='inner',lsuffix='left',rsuffix='right')# your analysis goes here...
 
-# load the necessary data here and transform to a UTM projection
+group_County_Ward = My_join.groupby(['CountyName','Population'])
+group_County_Ward['Population'].sum() # shows the total population per county and put into new column TotPop
 
-# your analysis goes here...
+max_County_Ward = group_County_Ward['Population'].max()
 
+print(f'highest population in a county is: {max_County_Ward}')
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
@@ -28,17 +33,47 @@ gridlines = ax.gridlines(draw_labels=True,
                          ylocs=[54, 54.5, 55, 55.5])
 gridlines.right_labels = False
 gridlines.bottom_labels = False
+def generate_handles(labels, colors, edge='k', alpha=1):
+    """
+    Generate matplotlib patch handles to create a legend of each of the features in the map.
+
+    Parameters
+    ----------
+
+    labels : list(str)
+        the text labels of the features to add to the legend
+
+    colors : list(matplotlib color)
+        the colors used for each of the features included in the map.
+
+    edge : matplotlib color (default: 'k')
+        the color to use for the edge of the legend patches.
+
+    alpha : float (default: 1.0)
+        the alpha value to use for the legend patches.
+
+    Returns
+    -------
+
+    handles : list(matplotlib.patches.Rectangle)
+        the list of legend patches to pass to ax.legend()
+    """
+    lc = len(colors)  # get the length of the color list
+    handles = [] # create an empty list
+    for ii in range(len(labels)): # for each label and color pair that we're given, make an empty box to pass to our legend
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
+    return handles
 
 # to make a nice colorbar that stays in line with our map, use these lines:
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
 # plot the ward data into our axis, using gdf.plot()
-ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
+ward_plot = Wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
 # add county outlines in red using ShapelyFeature
-county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
+county_outlines = ShapelyFeature(Counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
 ax.add_feature(county_outlines)
 
 county_handles = generate_handles([''], ['none'], edge='r')
@@ -48,3 +83,5 @@ ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', 
 
 # save the figure
 fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
+# print the figure ??
+fig
